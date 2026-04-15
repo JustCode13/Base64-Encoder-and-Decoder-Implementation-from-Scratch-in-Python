159658a5e382074c81759e4bd264312351ceafbd
codewithtea130 <tea858880@gmail.com>
2026-04-15 10:42:01 +0530

    feat: add constants.py with needed values and modes

---
+++ constants.py
@@ -0,0 +1,7 @@
+BASE64_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+BASE64_PADDING = "="
+
+VALID_MODES = ("encode", "decode")
+
+VALID_INPUT_KINDS= ("text","base64")
